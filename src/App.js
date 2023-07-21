dd6f77e25ff4b589ab0816091782e048760a8e38
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-07-21 20:59:46 +0600

    Chapter 07: Thinking In React State Management>> Lecture 7: Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const initialItems = [
 
 export default function App() {
   const [items, setItems] = useState(initialItems);
+
   function handleAddItems(item) {
     setItems((items) => [...items, item]); // immutable issue..... important.
   }
@@ -33,7 +34,7 @@ export default function App() {
         onDeleteItems={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -113,10 +114,23 @@ function Item({ item, onDeleteItems, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list 🚀</p>
+    );
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go"
+          : `You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
